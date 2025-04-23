d74dcca0845a4844772a830415190b9c97ddc34a
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-23 10:10:07 +0700

    Membuat tampilan informasi pesanan

--- lib/detail_order_page.dart
+++ lib/detail_order_page.dart
@@ -15,4 +15,24 @@ class DetailOrderPage extends StatelessWidget {
     required this.jumlahMinuman,
     required this.totalHarga,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+          ],
+        ),
+      ),
+    );
+  }
 }
