9aa901cd4dea6d7b1aa09ec152d33df52a274866
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-23 09:16:57 +0700

    Membuat Halaman RegisterPage

---
+++ lib/register_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {}
