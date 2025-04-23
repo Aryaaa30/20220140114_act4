b413a623cf06a69da331a527b02c9a0ed1cf58dc
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-23 09:34:22 +0700

    Menambahkan Tombol Register dan Navigasi ke Login

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -13,20 +13,20 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   void initState() {
-  super.initState();
+    super.initState();
   }
 
   @override
-Widget build(BuildContext context){
-  return Scaffold(
-    body: Form(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Text('Register'),
-            TextFormField(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Register'),
+              TextFormField(
                 controller: emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
                 validator: (value) {
@@ -47,11 +47,22 @@ Widget build(BuildContext context){
                   return null;
                 },
               ),
-          ]
-
-        )
-      )
-    )
-  )
-}
+              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                  // Navigator.pop(context);
+                  //Navigator.push(
+                  //  context,
+                  //  MaterialPageRoute(bulider: (context) => const LoginPage()),
+                  // );
+                },
+                child: Text('Sudah punya akun? Login Sekarang!'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
