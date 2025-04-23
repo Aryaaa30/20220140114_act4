1519bd5f14732be2b776dd01810c00ef7d092525
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-23 08:54:22 +0700

    Menambahkan Controller email dan password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,4 +6,12 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {}
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+}
